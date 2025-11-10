c7def54275b69559a0eab819d3472091e5a40cb8
MaikeraHW <maikuwagner@gmail.com>
2025-11-10 16:30:38 -0300

    first react class

--- Chronos/src/main.tsx
+++ Chronos/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./theme.css"
+import "./global.css"
+import { Container } from "./components/Container"
+// import { Heading } from "./components/Heading"
+// import { TimerIcon } from "lucide-react"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Container>Teste 01</Container>
+    <Container>Teste 02</Container>
+    <Container>Teste 03</Container>
+  </StrictMode>
 )
