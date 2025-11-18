3c5f2a99439de8208800d9097bf210084a1a05f8
MaikeraHW <maikuwagner@gmail.com>
2025-11-18 07:41:13 -0300

    page 404 - notFound created

--- Chronos/src/main.tsx
+++ Chronos/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Home } from "./pages/home"
+import { NotFound } from "./pages/NotFound"
+import { About } from "./pages/About"
 
 import "./theme.css"
 import "./global.css"
