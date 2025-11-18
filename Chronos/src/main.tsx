ca3f201e55837c969522bcb243ede91c286c91a6
MaikeraHW <maikuwagner@gmail.com>
2025-11-18 16:52:34 -0300

    react studies

--- Chronos/src/main.tsx
+++ Chronos/src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { Home } from "./pages/home"
-import { NotFound } from "./pages/NotFound"
-import { About } from "./pages/About"
 
 import "./theme.css"
 import "./global.css"
+import { App } from "./App"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
+    <App />
   </StrictMode>
 )
--- Chronos/src/pages/home/index.tsx
+++ Chronos/src/pages/home/index.tsx
@@ -1,9 +1,16 @@
 import { ClockTimer } from "../../components/ClockTimer"
 import { Container } from "../../components/Container"
 import { MainForm } from "../../components/MainForm"
+import type { TaskStateModel } from "../../models/TaskDateModel"
 import { MainTemplate } from "../../templates/MainTemplate/Index"
 
-export function Home() {
+type HomeProps = {
+  state: TaskStateModel
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>
+}
+
+export function Home(props: HomeProps) {
+  console.log(props)
   return (
     <MainTemplate>
       <Container>
