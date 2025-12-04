2c783fd092f9f1589b1f44ad7cd26ace18cd523c
MaikeraHW <maikuwagner@gmail.com>
2025-12-04 08:11:10 -0300

    devCurrency react studies continues

--- devcurrency/src/pages/home/index.tsx
+++ devcurrency/src/pages/home/index.tsx
@@ -1,14 +1,66 @@
 
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import type { FormEvent } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './home.module.css'
 
 export function Home() {
 
+  interface CoinProps{
+    id: string
+    rank: string
+    symbol: string
+    name: string
+    supply: string
+    maxSupply: string
+    masketCapUsd: string
+    volumeUsd24Hr: string
+    priceUsd: string
+    changePercent24Hr: string
+    vwap24Hr: string
+    explorer: string
+  }
+
+  interface DataProps{
+    data: CoinProps[]
+  }
+
+
+  const [input, setInput] = useState("")
+  const [coins, setCoins] = useState<CoinProps[]>([])
+
+
+    async function getData(){
+    fetch("https://rest.coincap.io/v3/assets?limit=10&offset=0&apiKey=d6abe85b5ef0212ecbfe0578366cd1e39c17776408857facd66a59f334b8008c")
+    .then(response => response.json())
+    .then((data: DataProps) => {
+      const coinsData = data.data
+      console.log(coinsData)
+    })
+  }
+
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const navigate = useNavigate();
+
+  function handleSubmit (e: FormEvent){
+    e.preventDefault()
+
+    if (input === ""){
+      return;
+    } else{
+      navigate(`/about/${input}`)
+    }
+  }
+
   return (
     <main className={styles.mainContent}>
       <form className={styles.mainForm}>
-        <input type="text" placeholder="Digite a moeda Ex.: Bitcoin" className={styles.mainInput}/>
-        <button type="submit" className={styles.mainButton}>Pesquisar</button>
+        <input type="text" placeholder="Digite a moeda Ex.: Bitcoin" className={styles.mainInput} onChange={(e) => { setInput(e.target.value)}}/>
+        <button type="submit" className={styles.mainButton} onClick={handleSubmit}>Pesquisar</button>
       </form>
 
       <table className={styles.table}>
@@ -35,6 +87,10 @@ export function Home() {
           </tr>
         </tbody>
       </table>
+
+      <button className={styles.buttonMore}>
+        Show more
+      </button>
     </main>
   )
 }
